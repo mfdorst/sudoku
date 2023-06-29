aeda0ed1871d7b0a4124d800b580babf99183475
Michael Dorst <m@mdorst.net>
2023-06-28 17:34:59 -0700

    Create sudoku table

--- src/Sudoku.jsx
+++ src/Sudoku.jsx
@@ -1,8 +1,69 @@
-
 function Sudoku() {
   return (
     <>
       <main className="sudoku">
+        <table>
+          <colgroup>
+            <col /> <col /> <col />
+          </colgroup>
+          <colgroup>
+            <col /> <col /> <col />
+          </colgroup>
+          <colgroup>
+            <col /> <col /> <col />
+          </colgroup>
+          <tbody>
+            <tr>
+              <td>1</td> <td>2</td> <td>3</td>
+              <td>4</td> <td>5</td> <td>6</td>
+              <td>7</td> <td>8</td> <td>9</td>
+            </tr>
+            <tr>
+              <td>1</td> <td>2</td> <td>3</td>
+              <td>4</td> <td>5</td> <td>6</td>
+              <td>7</td> <td>8</td> <td>9</td>
+            </tr>
+            <tr>
+              <td>1</td> <td>2</td> <td>3</td>
+              <td>4</td> <td>5</td> <td>6</td>
+              <td>7</td> <td>8</td> <td>9</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>1</td> <td>2</td> <td>3</td>
+              <td>4</td> <td>5</td> <td>6</td>
+              <td>7</td> <td>8</td> <td>9</td>
+            </tr>
+            <tr>
+              <td>1</td> <td>2</td> <td>3</td>
+              <td>4</td> <td>5</td> <td>6</td>
+              <td>7</td> <td>8</td> <td>9</td>
+            </tr>
+            <tr>
+              <td>1</td> <td>2</td> <td>3</td>
+              <td>4</td> <td>5</td> <td>6</td>
+              <td>7</td> <td>8</td> <td>9</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>1</td> <td>2</td> <td>3</td>
+              <td>4</td> <td>5</td> <td>6</td>
+              <td>7</td> <td>8</td> <td>9</td>
+            </tr>
+            <tr>
+              <td>1</td> <td>2</td> <td>3</td>
+              <td>4</td> <td>5</td> <td>6</td>
+              <td>7</td> <td>8</td> <td>9</td>
+            </tr>
+            <tr>
+              <td>1</td> <td>2</td> <td>3</td>
+              <td>4</td> <td>5</td> <td>6</td>
+              <td>7</td> <td>8</td> <td>9</td>
+            </tr>
+          </tbody>
+        </table>
       </main>
     </>
   );
