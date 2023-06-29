b9e3bc909cccf7dfa1c49defb51a2f724c660eef
Michael Dorst <m@mdorst.net>
2023-06-28 19:00:49 -0700

    Fetch boards from Dosuku and display them

--- src/Sudoku.jsx
+++ src/Sudoku.jsx
@@ -1,68 +1,71 @@
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+
 function Sudoku() {
+  const [gridData, setGridData] = useState(
+    Array(9)
+      .fill()
+      .map(() => ({
+        id: nanoid(),
+        row: Array(9)
+          .fill()
+          .map(() => ({ id: nanoid(), val: " " })),
+      }))
+  );
+
+  console.log(gridData);
+
+  const gridElements = gridData.map(({ id, row }) => (
+    <tr key={id}>
+      {row.map(({ id, val }) => (
+        <td key={id}>{val}</td>
+      ))}
+    </tr>
+  ));
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(
+        "https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value}}}"
+      );
+      let data = await res.json();
+      data = data.newboard.grids[0].value;
+      setGridData((gridData) =>
+        gridData.map(({ id, row }, i) => ({
+          id,
+          row: row
+            .map(({ id }, j) => ({
+              id,
+              val: data[i][j],
+            }))
+            .map(({ id, val }) => ({ id, val: val === 0 ? " " : `${val}` })),
+        }))
+      );
+    })();
+  }, []);
+
   return (
     <>
       <main className="sudoku">
         <table>
           <colgroup>
-            <col /> <col /> <col />
+            <col />
+            <col />
+            <col />
           </colgroup>
           <colgroup>
-            <col /> <col /> <col />
+            <col />
+            <col />
+            <col />
           </colgroup>
           <colgroup>
-            <col /> <col /> <col />
+            <col />
+            <col />
+            <col />
           </colgroup>
-          <tbody>
-            <tr>
-              <td>1</td> <td>2</td> <td>3</td>
-              <td>4</td> <td>5</td> <td>6</td>
-              <td>7</td> <td>8</td> <td>9</td>
-            </tr>
-            <tr>
-              <td>1</td> <td>2</td> <td>3</td>
-              <td>4</td> <td>5</td> <td>6</td>
-              <td>7</td> <td>8</td> <td>9</td>
-            </tr>
-            <tr>
-              <td>1</td> <td>2</td> <td>3</td>
-              <td>4</td> <td>5</td> <td>6</td>
-              <td>7</td> <td>8</td> <td>9</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>1</td> <td>2</td> <td>3</td>
-              <td>4</td> <td>5</td> <td>6</td>
-              <td>7</td> <td>8</td> <td>9</td>
-            </tr>
-            <tr>
-              <td>1</td> <td>2</td> <td>3</td>
-              <td>4</td> <td>5</td> <td>6</td>
-              <td>7</td> <td>8</td> <td>9</td>
-            </tr>
-            <tr>
-              <td>1</td> <td>2</td> <td>3</td>
-              <td>4</td> <td>5</td> <td>6</td>
-              <td>7</td> <td>8</td> <td>9</td>
-            </tr>
-          </tbody>
-          <tbody>
-            <tr>
-              <td>1</td> <td>2</td> <td>3</td>
-              <td>4</td> <td>5</td> <td>6</td>
-              <td>7</td> <td>8</td> <td>9</td>
-            </tr>
-            <tr>
-              <td>1</td> <td>2</td> <td>3</td>
-              <td>4</td> <td>5</td> <td>6</td>
-              <td>7</td> <td>8</td> <td>9</td>
-            </tr>
-            <tr>
-              <td>1</td> <td>2</td> <td>3</td>
-              <td>4</td> <td>5</td> <td>6</td>
-              <td>7</td> <td>8</td> <td>9</td>
-            </tr>
-          </tbody>
+          <tbody>{gridElements.slice(0, 3)}</tbody>
+          <tbody>{gridElements.slice(3, 6)}</tbody>
+          <tbody>{gridElements.slice(6, 9)}</tbody>
         </table>
       </main>
     </>
