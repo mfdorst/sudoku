f1a6a0ab5148a830945fc28bf19c87e8fe82a533
Michael Dorst <m@mdorst.net>
2024-02-21 12:52:50 -0800

    Refactor isBoardCorrect

--- src/game_logic.js
+++ src/game_logic.js
@@ -1,33 +1,10 @@
-export function isBoardCorrect(board) {
-  if (!isBoardFilled) return false;
-  if (!board.every((row) => isValidSequence(row))) return false;
-  const transposedBoard = board.map((_, i) => board.map((row) => row[i]));
-  if (!transposedBoard.every((col) => isValidSequence(col))) return false;
-  const boxCoords = [0, 1, 2].flatMap((x) => [0, 1, 2].map((y) => [x, y]));
-  if (!boxCoords.every(([x, y]) => isValidSequence(getBox(x, y, board))))
-    return false;
-  return true;
-}
+export function isBoardCorrect(grid) {
+  const checkNoDuplicates = arr => new Set(arr).size === arr.length && arr.every(val => val !== " ");
 
-function getBox(x, y, board) {
-  const rows = board.slice(y * 3, y * 3 + 3);
-  return rows.flatMap((row) => row.slice(x * 3, x * 3 + 3));
-}
-
-function isBoardFilled(board) {
-  return board.every((row) =>
-    row.every((cell) => {
-      const val = cell.map(Number);
-      return val >= 1 && val <= 9;
-    })
-  );
-}
-
-// Verify that the sequence has all numbers 1-9
-function isValidSequence(sequence) {
-  const bitfield = sequence
-    .map(Number)
-    .reduce((acc, val) => acc | (1 << (val - 1)), 0);
-  // 11111111bin == 511dec
-  return bitfield === 511;
+  return grid.every(checkNoDuplicates) &&
+    [...Array(9).keys()].every(col => checkNoDuplicates(grid.map(row => row[col]))) &&
+    [0, 1, 2].every(row => [0, 1, 2].every(col =>
+      checkNoDuplicates(grid.slice(row * 3, row * 3 + 3)
+                            .flatMap(row => row.slice(col * 3, col * 3 + 3)))
+    ));
 }
